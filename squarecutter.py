a8b52321eae12987f7e4f503609e5c53b21ed602
Caro Silva Rode <83190363+budindepunk@users.noreply.github.com>
2024-01-03 11:17:37 +0100

    readme and small fixes

--- squarecutter.py
+++ squarecutter.py
@@ -2,6 +2,9 @@ from PIL import Image
 
 kirby = Image.open("sun.png")
 
+
+# image is 300px x 300px, this cuts it based on that.
+# ideally this is changed to 1/3 instead of an absolute coordinate so that it can work with differently-sized images
 box1 = (0, 0, 100, 100)
 box2 = (100, 0, 200, 100)
 box3 = (200, 0, 300, 100)
